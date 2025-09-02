f5396373b012ca897abddd4a389e2bc218ad87c2
Gabryel Siqueira Navarro <gabryelsn2001@gmail.com>
2025-09-01 23:56:05 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
     });
 
     try {
-      const response = await fetch("http://localhost:5000/paciente", {
+      const response = await fetch("https://softwarepsi-1.onrender.com/paciente", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
