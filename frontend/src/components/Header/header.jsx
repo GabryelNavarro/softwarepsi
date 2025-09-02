6de68d04ba41c7150c58530560b877c7fa401fef
Gabryel Siqueira Navarro <gabryelsn2001@gmail.com>
2025-09-02 14:24:06 -0300

    new

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -4,12 +4,14 @@ import "../styles/reset.css"
 import yasmnin from "../Header/img/Yasmin.svg"
 import psi_plano from "../Header/img/psi_plano.svg"
 import"../styles/reset.css"
+import yasmin_nome from "../Header/img/yasmin_nome_completo.svg"
 
 
 function Header() {
   return (
-    <header>
-      <img src={plano_fundo} alt="imagem_plano" className="plano_fundo" />
+    <header className="header">
+
+      <img src={yasmin_nome} alt="" className="yasmin_completo"/>
       <img src= {yasmnin} alt="yasmin" className="yasmin__img" />
       <img src={psi_plano} alt="psi plano" className="psi__plano" />
     </header>
